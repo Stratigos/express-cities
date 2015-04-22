c5efbfcfd053ad75445715940b50872ec6966547
Stratigos <Stratigos@users.noreply.github.com>
2015-04-22 07:52:58 -0400

    added www script to launch app; moved port listening to www script; added initial sample test; added supertest package;

--- app.js
+++ app.js
@@ -5,4 +5,7 @@ app.get('/', function(reqiest, response) {
   response.send('OK');
 });
 
-app.listen(3000);
+// encapsulate app inside of node module, in order to allow app definition and
+//  the code that binds the app to the network to exist in different
+//  files/modules (this replaces `app.listen(3000);`).
+module.exports = app;
---
+++ test.js
@@ -0,0 +1,15 @@
+/**
+ * TODO - DOCUMENTATION
+ */
+var request = require('supertest');
+var app     = require('./app');
+
+request(app)
+  .get('/')
+  .expect(200)
+  .end(function(error){
+    if(error) {
+      throw error;
+    }
+    console.log("\n~~FINISHED TESTS~~\n");
+});
