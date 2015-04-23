cb4b7d63cddf5fc5cca1bfbed08beaad954acde1
Stratigos <Stratigos@users.noreply.github.com>
2015-04-22 20:32:33 -0400

    sample test converted to mocha test; updated package.json to add test command;

--- test.js
+++ test.js
@@ -4,12 +4,17 @@
 var request = require('supertest');
 var app     = require('./app');
 
-request(app)
-  .get('/')
-  .expect(200)
-  .end(function(error){
-    if(error) {
-      throw error;
-    }
-    console.log("\n~~FINISHED TESTS~~\n");
+describe('Requests to Root Path', function() {
+  it('Returns a HTTP Status Code: 200', function(done) {
+    request(app)
+      .get('/')
+      .expect(200)
+      .end(function(error){
+        if(error) {
+          throw error;
+        }
+        done();
+    });
+  });
 });
+
