4757080ae6663856f0e17f75b1df302ba8bceee2
Stratigos <Stratigos@users.noreply.github.com>
2015-04-25 11:27:02 -0400

    zeroing in on stupid typo mucking up heroku redis

--- app.js
+++ app.js
@@ -17,7 +17,8 @@ var urlEncode  = bodyParser.urlencoded({extended: false});
 var redis  = require('redis');
 if (process.env.REDISTOGO_URL) {
   var rtg    = require('url').parse(process.env.REDISTOGO_URL);
-  var cleint = redis.createClient(rtg.port, rtg.hostname);
+  console.log("\n RTG PORT AND HOSTNAME: \n", rtg.port, rtg.hostname, "\n");
+  var client = redis.createClient(rtg.port, rtg.hostname);
   console.log("\n REDIS CLIENT: ", client, "\n!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n")
 
   cleint.auth(rtg.auth.split(":")[1]);
